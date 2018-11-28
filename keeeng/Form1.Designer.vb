da7948a0351e02f147fa95201c7a9a62b6ea97e2
ecnivlinise <32978709+ecnivlinise@users.noreply.github.com>
2018-11-28 20:05:50 +0100

    Fichiers

---
+++ keeeng/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FixerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ÉditéToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 24)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(351, 426)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Tomato
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FixerToolStripMenuItem, Me.ÉditéToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(351, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FixerToolStripMenuItem
+        '
+        Me.FixerToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.FixerToolStripMenuItem.Name = "FixerToolStripMenuItem"
+        Me.FixerToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.FixerToolStripMenuItem.Text = "Détacher"
+        '
+        'ÉditéToolStripMenuItem
+        '
+        Me.ÉditéToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ÉditéToolStripMenuItem.Name = "ÉditéToolStripMenuItem"
+        Me.ÉditéToolStripMenuItem.Size = New System.Drawing.Size(55, 20)
+        Me.ÉditéToolStripMenuItem.Text = "Ouvert"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(351, 450)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Keeeng"
+        Me.TopMost = True
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FixerToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ÉditéToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ keeeng/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub FixerToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FixerToolStripMenuItem.Click
+        If (My.Settings.fixer = True) Then
+            Me.TopMost = False
+            FixerToolStripMenuItem.Text = ("Fixer")
+            My.Settings.fixer = False
+            My.Settings.Save()
+        ElseIf (My.Settings.fixer = False) Then
+            Me.TopMost = True
+            FixerToolStripMenuItem.Text = ("Détacher")
+            My.Settings.fixer = True
+            My.Settings.Save()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RichTextBox1.Text = My.Settings.text
+        If (My.Settings.fixer = True) Then
+            Me.TopMost = True
+        End If
+        If (My.Settings.fixer = False) Then
+            Me.TopMost = False
+        End If
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+        ÉditéToolStripMenuItem.Text = ("Sauvegarde...")
+        My.Settings.text = (RichTextBox1.Text)
+        My.Settings.Save()
+        ÉditéToolStripMenuItem.Text = ("Sauvegardé")
+    End Sub
+End Class
